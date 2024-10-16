236c34764e3eaef568f8e7a187c4f846de95a07e
Taufik Hidayat <tfkhdyt@proton.me>
2024-10-16 10:05:47 +0700

    docs: add JSDoc

--- mod.ts
+++ mod.ts
@@ -1,3 +1,14 @@
+/**
+ * Handles a promise by catching specific errors and returning either the result or the error.
+ *
+ * @template T - The type of the resolved value of the promise.
+ * @template E - The error type to catch. Must extend the `Error` class.
+ *
+ * @param {Promise<T>} promise - The promise to be resolved or rejected.
+ * @param {E[]} [errorsToCatch] - Optional list of error types to catch. If the thrown error is not in this list, it will be rethrown.
+ *
+ * @returns {Promise<[undefined, T] | [InstanceType<E>]>} A tuple where the first element is `undefined` if no error occurred, and the second is the resolved value of the promise. If an error is caught, the tuple contains the error instance.
+ */
 export async function withCatch<T, E extends new (message?: string) => Error>(
   promise: Promise<T>,
   errorsToCatch?: E[]
@@ -11,6 +22,17 @@ export async function withCatch<T, E extends new (message?: string) => Error>(
   }
 }
 
+/**
+ * Executes a synchronous callback and catches specific errors, returning either the result or the error.
+ *
+ * @template T - The return type of the callback.
+ * @template E - The error type to catch. Must extend the `Error` class.
+ *
+ * @param {(...args: any[]) => T} callback - The function to be executed synchronously.
+ * @param {E[]} [errorsToCatch] - Optional list of error types to catch. If the thrown error is not in this list, it will be rethrown.
+ *
+ * @returns {[undefined, T] | [InstanceType<E>]} A tuple where the first element is `undefined` if no error occurred, and the second is the result of the callback. If an error is caught, the tuple contains the error instance.
+ */
 export function withCatchSync<T, E extends new (message?: string) => Error>(
   callback: (...args: any[]) => T,
   errorsToCatch?: E[]
